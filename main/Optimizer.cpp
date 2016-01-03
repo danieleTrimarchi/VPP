c5cf9e84cd9ae60a66040f9adeef04e56468551d
Daniele Trimarchi <daniele.trimarchi@gmail.com>
2016-01-03 22:30:42 +0100

    With benchmarck that does not converge to the expected value...

--- main/Optimizer.cpp
+++ main/Optimizer.cpp
@@ -144,37 +144,29 @@ void Optimizer::runExample2() {
 }
 
 // Try with ISRES global optimization
-void Optimizer::run() {
+void Optimizer::runISRES() {
 
+	// Set the dimension of this problem
+	size_t dimension=2;
 
   // Instantiate a NLOpobject and set the ISRES "Improved Stochastic Ranking Evolution Strategy"
 	// algorithm for nonlinearly-constrained global optimization
 	///GN_ISRES
-	nlopt::opt opt(nlopt::GN_ISRES,2);
+	nlopt::opt opt(nlopt::GN_ISRES,dimension);
 
-//	This produce an exception for an invalid argument.
-//	See the example for DIRECT-L global optimisation algorithm in
-//	/Users/daniele/third_party/nlopt-2.4.2/test/testopt.cpp
-//	and see if from that I can drive other global optimisation algs!
-//	... then I still need to find a way to apply the alg to the VPP eqs!
-
- 	// Set the and apply the lower and the upper bounds
+ 	// Set the and apply the lower and the upper bounds for the constraints
+	// -> make sure the bounds are larger than the initial
+	// 		guess!
    std::vector<double> lb(2),ub(2);
-   lb[0] = 0;
-   lb[1] = 0;
-   ub[0] = 10.;
-   ub[1] = 10.;
+   for(size_t i=0; i<2; i++){
+  	 lb[i] = 0;
+  	 ub[i] = 6.;
+   }
 
+   // Set the bounds for the constraints
    opt.set_lower_bounds(lb);
    opt.set_upper_bounds(ub);
 
-   std::vector<double> lboundCheck = opt.get_lower_bounds();
-   std::vector<double> uboundCheck = opt.get_upper_bounds();
-
-   for(size_t i=0; i<lboundCheck.size(); i++){
-  	 std::cout<<"lboundCheck["<<i<<"]= "<<lboundCheck[i]<<std::endl;
-  	 std::cout<<"uboundCheck["<<i<<"]= "<<uboundCheck[i]<<"\n---\n"<<std::endl;
-   }
    // Set the objective function to be minimized (or maximized, using set_max_objective)
    opt.set_min_objective(myfunc, NULL);
 
@@ -189,8 +181,9 @@ void Optimizer::run() {
    // Set the relative tolerance
    opt.set_xtol_rel(1e-4);
 
-   // Set some initial guess
-   std::vector<double> xp(2);
+   // Set some initial guess. Make sure it is within the
+   // bounds that have been set
+   std::vector<double> xp(dimension);
    xp[0]= 1.234;
    xp[1]= 5.678;
 
@@ -209,4 +202,114 @@ void Optimizer::run() {
    }
 
 }
+
+// Set the objective function and gradient for example1:
+// Find min : sqrt(x2)
+// Subjected to y>0, y>=(a1*x + b1)^3 , y>=(a2*x +b2)^3
+double Optimizer::myfunc_g06(unsigned n, const double *x, double *grad, void *my_func_data) {
+
+		// Increment the number of iterations for each call of the objective function
+		++optIterations;
+
+    return (std::pow((x[0]-10),3) + std::pow((x[1]-20),3) );
+
+}
+
+// Set the constraint function and gradient for example1:
+// Set the inequality in the shape: (-1)^s1 (x1 + a)^2 + (-1)^s2 (x2 + b)^2 + c ≤ 0
+double Optimizer::myconstraint_g06(unsigned n, const double *x, double *grad, void *data) {
+
+	g06_constraint_data *d = (g06_constraint_data *) data;
+
+	return (
+					std::pow((-1),d->s1) * std::pow(x[0]+d->a,2) +
+					std::pow((-1),d->s2) * std::pow(x[1]+d->b,2) +
+					d->c
+				);
+}
+
+
+void Optimizer::run() {
+
+	// g06:
+	// Test function: f(⃗x) = (x1 − 10)^3 + (x2 − 20)^3
+	// Constraints: g1(x) : −(x1 −5)^2 − (x2 −5)^2 + 100 ≤ 0
+	//					 		g2(x) :  (x1 −6)^2 + (x2 −5)^2 − 82.81 ≤ 0
+	// where 13 ≤ x1 ≤ 100 and 0 ≤ x2 ≤ 100.
+	// The optimum solution is x = (14.095, 0.84296)
+	// where f(x) = −6961.81388
+
+	// Set the dimension of this problem
+	size_t dimension=3;
+
+  // Instantiate a NLOpobject and set the ISRES "Improved Stochastic Ranking Evolution Strategy"
+	// algorithm for nonlinearly-constrained global optimization
+	///GN_ISRES
+	nlopt::opt opt(nlopt::GN_ISRES,dimension);
+
+ 	// Set the and apply the lower and the upper bounds
+	// -> make sure the bounds are larger than the initial
+	// 		guess!
+
+   std::vector<double> lb(dimension),ub(dimension);
+  	 lb[0] = 13;
+  	 ub[0] = 100;
+  	 lb[1] = 0;
+  	 ub[1] = 100;
+
+   // Set the bounds for the constraints
+   opt.set_lower_bounds(lb);
+   opt.set_upper_bounds(ub);
+
+   // Set the objective function to be minimized (or maximized, using set_max_objective)
+   opt.set_min_objective(myfunc_g06, NULL);
+
+   // Set the constraint equations
+   g06_constraint_data data[2] = { {-1,-5,-1,-5,+100}, {1,-6,1,-5,-82.81} };
+
+   opt.add_inequality_constraint(myconstraint_g06, &data[0], 1e-8);
+   opt.add_inequality_constraint(myconstraint_g06, &data[1], 1e-8);
+
+   // Set the relative tolerance
+   opt.set_xtol_rel(1e-4);
+
+   opt.set_maxeval(500000);
+
+   // Set some initial guess. Make sure it is within the
+   // bounds that have been set
+   std::vector<double> xp(dimension);
+   xp[0]= 14;
+   xp[1]= 0.8;
+
+   // Instantiate the minimum objective value, upon return
+   double minf;
+
+   // Launch the optimization; negative retVal implies failure
+   nlopt::result result = opt.optimize(xp, minf);
+
+   if (result < 0) {
+       printf("nlopt failed!\n");
+   }
+   else {
+   		printf("found minimum after %d evaluations\n", optIterations);
+       printf("found minimum at f(%g,%g) = %0.10g\n", xp[0], xp[1], minf);
+   }
+
+
+	/////////////////////////////////////////
+	// g04:
+	// Test function : f(x) = 5.3578547 (x3)^2 + 0.8356891 x1 x5 +  37.293239 x1 - 40792.141
+	// Subjected to :
+	// g1(x) = 85.334407 + 0.0056858 x2 x5 + 0.0006262 x1 x4 − 0.0022053 x3 x5 − 92 ≤ 0
+	// g2(x) = −85.334407 − 0.0056858 x2 x5 − 0.0006262 x1 x4 + 0.0022053 x3 x5 ≤ 0
+	// g3(x) = 80.51249 + 0.0071317 x2 x5 + 0.0029955 x1 x2 + 0.0021813 (x3)^2 − 110 ≤ 0
+	// g4(x) = −80.51249 − 0.0071317 x2 x5 − 0.0029955 x1 x2 − 0.0021813 (x3)^2 + 90 ≤ 0
+	// g5(x) = 9.300961 + 0.0047026 x3 x5 + 0.0012547 x1 x3 + 0.0019085 x3 x4 − 25 ≤ 0
+	// g6(x) = −9.300961 − 0.0047026 x3 x5 − 0.0012547 x1 x3 − 0.0019085 x3 x4 + 20 ≤ 0
+	// where 78≤x1≤102 , 33≤x2≤45 and 27≤xi≤45 (i=3,4,5).
+	// The optimum solution is x =(78, 33, 29.995256025682, 45, 36.775812905788)
+	// where f(x) = −30665.539.
+
+}
+
 
--- main/Optimizer.h
+++ main/Optimizer.h
@@ -28,6 +28,11 @@ class Optimizer {
 		/// Execute example 2
 		void runExample2();
 
+		/// Execute an example running ISRES "Improved Stochastic Ranking Evolution Strategy"
+		/// this is the global optimization algorithm with non-linear equality constraints we
+		/// can use for the VPP
+		void runISRES();
+
 		/// Execute
 		void run();
 
@@ -36,14 +41,30 @@ class Optimizer {
 		/// Function required for example1. Set the objective function
 		static double myfunc(unsigned n, const double *x, double *grad, void *my_func_data);
 
+		// Same but for benchmark g06
+		static double myfunc_g06(unsigned n, const double *x, double *grad, void *my_func_data);
+
 		/// Function requested for example1. Set the constraint function
 		static double myconstraint(unsigned n, const double *x, double *grad, void *data);
+
+		/// Function requested for benchmark g06
+		static double myconstraint_g06(unsigned n, const double *x, double *grad, void *data);
 
 		/// Struct requested by example1. Coefficient for each constraint in the
 		/// shape : y >= (ax+b)^3
 		typedef struct {
 		    double a, b;
 		} my_constraint_data;
+
+		/// Struct requested by example1. Coefficient for each constraint in the
+		/// shape : y >= (ax+b)^3
+		typedef struct {
+
+				int s1;
+		    double a;
+				int s2;
+				double b, c;
+		} g06_constraint_data;
 
 };
 
