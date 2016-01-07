8791786e93cc6a832901406e8a6fa7c1e7d286d9
Daniele Trimarchi <daniele.trimarchi@gmail.com>
2016-01-07 18:56:05 +0100

    Builds with the beginning of the VPPItem structure

--- main/VPPItem.cpp
+++ main/VPPItem.cpp
@@ -10,7 +10,17 @@ b_(0),
 f_(0) {
 
 	// Instantiate the wind
-	vppItems_.push_back(boost::shared_ptr(new WindItem()) );
+	boost::shared_ptr<WindItem> pWind(new WindItem(pParser_,pSailSet_));
+
+	// Push it back to the children vector
+	vppItems_.push_back( pWind );
+
+	// Instantiate the sail coefficients
+	boost::shared_ptr<SailCoefficientItem> pSailCoeffItem(new SailCoefficientItem(pWind.get()));
+
+	// Push it back to the children vector
+	vppItems_.push_back( pSailCoeffItem );
+
 }
 
 // Destructor
@@ -19,12 +29,12 @@ VPPItem::~VPPItem() {
 }
 
 // Returns the parser
-VariableFileParser* VPPItem::getParser() {
+VariableFileParser* VPPItem::getParser() const {
 	return pParser_;
 }
 
 // Returns the SailSet
-boost::shared_ptr<SailSet> VPPItem::getSailSet() {
+boost::shared_ptr<SailSet> VPPItem::getSailSet() const {
 	return pSailSet_;
 }
 
@@ -44,6 +54,10 @@ void VPPItem::update(int vTW, int aTW, const double* x) {
 
 }
 
+// Implement the pure virtual to allow instantiating this class
+void VPPItem::update(int vTW, int aTW) {
+	// make nothing
+}
 
 void VPPItem::printWhoAmI() {
 	std::cout<<"--> WhoAmI of VPPItem "<<std::endl;
@@ -53,12 +67,13 @@ void VPPItem::printWhoAmI() {
 //=========================================================
 
 /// Constructor
-WindItem::WindItem() :
-		twv_(0),
-		twa_(0),
-		awa_(0),
-		awv_(Eigen::Vector2d::Zero()),
-		pi_(M_PI / 180.0) {
+WindItem::WindItem(VariableFileParser* pParser, boost::shared_ptr<SailSet> pSailSet) :
+				VPPItem(pParser,pSailSet),
+				twv_(0),
+				twa_(0),
+				awa_(0),
+				awv_(Eigen::Vector2d::Zero()),
+				pi_(M_PI / 180.0){
 
 }
 
@@ -72,6 +87,9 @@ void WindItem::update(int vTW, int aTW) {
 
 	// Get a reference to the varSet
 	const VarSet& v = *(pParser_->getVariables());
+
+	// todo dtrimarchi : it this method is called n times by the optimizer, so it is
+	// stupid to re-compute twv_ e twa_ at each time!
 
 	// Update the true wind velocity
 	// vmin to vmax in N steps : vMin + vTW * ( (vMax-vMin)/(nSteps-2) - 1 )
@@ -94,4 +112,110 @@ void WindItem::printWhoAmI() {
 	std::cout<<"--> WhoAmI of WindItem "<<std::endl;
 }
 
+/// Returns the true wind velocity for this step
+const double WindItem::getTWV() const {
+	return twv_;
+}
+
+/// Returns the true wind angle for this step
+const double WindItem::getTWA() const {
+	return twa_;
+}
+
+/// Returns the apparent wind angle for this step
+const double WindItem::getAWA() const {
+	return awa_;
+}
+
+/// Returns the apparent wind velocity vector for this step
+const Eigen::Vector2d WindItem::getAWV() const {
+	return awv_;
+}
+
+//=================================================================
+
+// Constructor
+SailCoefficientItem::SailCoefficientItem(WindItem* pWindItem) :
+				VPPItem(pWindItem->getParser(), pWindItem->getSailSet()),
+				pWindItem_(pWindItem) {
+
+	// Init static members with values from: Hazer Cl-Cd coefficients, 1999
+	// Cols: rwa_, Main_Cl, Jib_Cl, Spi_Cl
+
+	// Pick the lift coefficient for this main (full batten or not)
+	if( pParser_->get("MFBL") ) {
+
+		cl_.resize(6,4);
+		cl_.row(0) << 0, 		0, 			0, 		0   ;
+		cl_.row(1) << 27, 	1.725,	1.5,	0   ;
+		cl_.row(2) << 50, 	1.5, 		0.5,	1.5 ;
+		cl_.row(3) << 80,		0.95, 	0.3,	1.0 ;
+		cl_.row(4) << 100,	0.85, 	0.0,	0.85;
+		cl_.row(5) << 180,	0, 			0, 		0		;
+
+	} else {
+
+		cl_.resize(6,4);
+		cl_.row(0) << 0, 		0, 	 	0 , 	0;
+		cl_.row(1) << 27, 	1.5, 	1.5,	0;
+		cl_.row(2) << 50, 	1.5, 	0.5,	1.5;
+		cl_.row(3) << 80, 	0.95,	0.3,	1.0;
+		cl_.row(4) << 100,	0.85,	0.0,	0.85;
+		cl_.row(5) << 180,	0, 	 	0,	 	0;
+
+	}
+
+	// We only dispose of one drag coeff array for the moment
+	cd_.resize(6,4);
+	cl_.row(0) << 0,  	0,   	0,   	0;
+	cl_.row(1) << 27, 	0.02,	0.02,	0;
+	cl_.row(2) << 50, 	0.15,	0.25,	0.25;
+	cl_.row(3) << 80, 	0.8, 	0.15,	0.9;
+	cl_.row(4) << 100,	1.0, 	0.0, 	1.2;
+	cl_.row(5) << 180,	0.9, 	0.0, 	0.66;
+
+}
+
+// Destructor
+SailCoefficientItem::~SailCoefficientItem() {
+
+}
+
+void SailCoefficientItem::update(int vTW, int aTW) {
+
+	/// Get the current value of the apparent wind angle
+	double awa= pWindItem_->getAWA();
+
+}
+
+// Print the class name -> in this case SailCoefficientItem
+void SailCoefficientItem::printWhoAmI() {
+	std::cout<<"--> WhoAmI of SailCoefficientItem "<<std::endl;
+}
+
+//=================================================================
+
+// Constructor
+AeroForcesItem::AeroForcesItem(WindItem* pWindItem) :
+		VPPItem(pWindItem->getParser(), pWindItem->getSailSet() ),
+		pWindItem_(pWindItem) {
+
+	// Instantiate the sail coefficients
+	pSailCoeffs_= boost::shared_ptr<SailCoefficientItem>(new SailCoefficientItem(pWindItem_));
+}
+
+// Destructor
+AeroForcesItem::~AeroForcesItem() {
+
+}
+
+/// Update the items for the current step (wind velocity and angle)
+void AeroForcesItem::update(int vTW, int aTW) {
+
+
+}
+
+void AeroForcesItem::printWhoAmI() {
+	std::cout<<"--> WhoAmI of AeroForcesItem "<<std::endl;
+}
 
--- main/VPPItem.h
+++ main/VPPItem.h
@@ -30,13 +30,13 @@ class VPPItem {
 		/// Print the class name -> in this case VPPItem
 		virtual void printWhoAmI();
 
+		/// Returns a ptr to the parser
+		VariableFileParser* getParser() const;
+
+		/// Returns a ptr to the SailSet
+		boost::shared_ptr<SailSet> getSailSet() const;
+
 	protected:
-
-		/// Returns the parser
-		VariableFileParser* getParser();
-
-		/// Returns the SailSet
-		boost::shared_ptr<SailSet> getSailSet();
 
 		/// Velocity value in the state vector
 		double V_;
@@ -50,17 +50,17 @@ class VPPItem {
 		/// Flat value in the state vector
 		double f_;
 
-	private:
-
-		/// Update the items for the current step (wind velocity and angle),
-		/// the value of the state vector x computed by the optimizer
-		void update(int vTW, int aTW) =0;
-
 		/// Ptr to the file parser with the user variables
 		VariableFileParser* pParser_;
 
 		/// Ptr to the SailSet with the sail related variables
 		boost::shared_ptr<SailSet> pSailSet_;
+
+	private:
+
+		/// Update the items for the current step (wind velocity and angle),
+		/// the value of the state vector x computed by the optimizer
+		virtual void update(int vTW, int aTW) =0;
 
 		/// Vector of VPPItem ptrs
 		vector<boost::shared_ptr<VPPItem> > vppItems_;
@@ -74,13 +74,27 @@ class WindItem : public VPPItem {
 	public:
 
 		/// Constructor
-		WindItem();
+		WindItem(VariableFileParser*, boost::shared_ptr<SailSet>);
 
 		/// Destructor
 		virtual ~WindItem();
 
 		/// Print the class name -> in this case WindItem
 		virtual void printWhoAmI();
+
+		/// Own methods:
+
+		/// Returns the true wind velocity for this step
+		const double getTWV() const;
+
+		/// Returns the true wind angle for this step
+		const double getTWA() const;
+
+		/// Returns the apparent wind angle for this step
+		const double getAWA() const;
+
+		/// Returns the apparent wind velocity vector for this step
+		const Eigen::Vector2d getAWV() const;
 
 	private:
 
@@ -103,5 +117,67 @@ class WindItem : public VPPItem {
 
 };
 
+//=================================================================
+
+/// Utility class used by the AeroForcesItem to store the sail coefficients
+/// the class is able to interpolate the coefficients for the current apparent
+/// wind angle (computed by WindItem)
+class SailCoefficientItem : public VPPItem {
+
+	public:
+
+		/// Constructor
+		SailCoefficientItem(WindItem*);
+
+		/// Destructor
+		~SailCoefficientItem();
+
+		/// Print the class name -> in this case SailCoefficientItem
+		virtual void printWhoAmI();
+
+	private:
+
+		/// Update the item for the current step (wind velocity and angle),
+		/// the values of the state vector x computed by the optimizer have
+		/// already been treated by the parent
+		virtual void update(int vTW, int aTW);
+
+		/// static arrays with the sail coefficients
+		Eigen::ArrayXd cl_,cd_;
+
+		/// Ptr to the wind item
+		WindItem* pWindItem_;
+
+};
+
+//=================================================================
+
+class AeroForcesItem : public VPPItem {
+
+	public:
+
+		/// Constructor
+		AeroForcesItem(WindItem*);
+
+		/// Destructor
+		virtual ~AeroForcesItem();
+
+		/// Print the class name -> in this case AeroForcesItem
+		virtual void printWhoAmI();
+
+	private:
+
+		/// Update the item for the current step (wind velocity and angle),
+		/// the values of the state vector x computed by the optimizer have
+		/// already been treated by the parent
+		virtual void update(int vTW, int aTW);
+
+		/// Store a ptr to the windItem
+		WindItem* pWindItem_;
+
+		/// The AeroForcesItem owns the sail coefficients
+		boost::shared_ptr<SailCoefficientItem> pSailCoeffs_;
+
+};
 
 #endif
