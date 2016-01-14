3a62f682d9c33bfab7e4a88314c4588aafaafa6d
Daniele Trimarchi <daniele.trimarchi@gmail.com>
2016-01-14 19:22:03 +0100

    Added Residuary resistance

--- main/VPPHydroItem.cpp
+++ main/VPPHydroItem.cpp
@@ -1,9 +1,23 @@
 #include "VPPHydroItem.h"
+#include "Warning.h"
 
 // Constructor
 ResistanceItem::ResistanceItem(VariableFileParser* pParser, boost::shared_ptr<SailSet> pSailSet) :
-	VPPItem(pParser,pSailSet) {
+	VPPItem(pParser,pSailSet),
+	fN_(0) {
 	// make nothing
+}
+
+// drag the pure virtual method of the parent class one step down
+void ResistanceItem::update(int vTW, int aTW) {
+
+	// Update the Froude number using the state variable boat velocity
+	fN_= fabs(V_) / sqrt(Physic::g * pParser_->get("LWL"));
+
+	if(fN_ > 0.6)
+		Warning warning("The value of the Froude Number exceeds the limit value 0.6! "
+				"Results might be incorrect.");
+
 }
 
 // Destructor
@@ -57,17 +71,11 @@ InducedResistanceItem::~InducedResistanceItem() {
 // Implement pure virtual method of the parent class
 void InducedResistanceItem::update(int vTW, int aTW) {
 
-	// Get the Froude number using the state variable boat velocity
-	double Fn= fabs(V_) / sqrt(Physic::g * pParser_->get("LWL"));
-
-  if(Fn > 0.6)
-  	Fn = 0.6; // todo dtrimarchi: understand this comment:
-  						// this is quite a rough approximation, but
-  						// over Fn 0.6 everything is roughly approximated...
-  						// and this prevent Te to become negative!
+	// Call the parent class update to update the Froude number
+	ResistanceItem::update(vTW,aTW);
 
   Eigen::ArrayXd vectB(2);
-  vectB << 1, Fn;
+  vectB << 1, fN_;
 
   // coeffB(4x2) * vectB'(2x1) => TeFn(4x1)
   // todo dtrimarchi - WARNING : is this going to do the right
@@ -90,4 +98,82 @@ void InducedResistanceItem::update(int vTW, int aTW) {
 
 }
 
+//=================================================================
+
+// Constructor
+ResiduaryResistanceItem::ResiduaryResistanceItem(VariableFileParser* pParser, boost::shared_ptr<SailSet> pSailSet):
+		ResistanceItem(pParser, pSailSet),
+		rrh_(0) {
+
+	// Define an array of coefficients and instantiate an interpolator over it
+	Eigen::MatrixXd coeff(11,9);
+	coeff << 	-0.0014,  0.0403,  0.0470, -0.0227, -0.0119,  0.0061, -0.0086, -0.0307, -0.0553,
+						 0.0004, -0.1808,  0.1793, -0.0004,  0.0097,  0.0118, -0.0055,  0.1721, -0.1728,
+             0.0014, -0.1071,  0.0637,  0.0090,  0.0153,  0.0011,  0.0012,  0.1021, -0.0648,
+             0.0027,  0.0463, -0.1263,  0.0150,  0.0274, -0.0299,  0.0110, -0.0595,  0.1220,
+             0.0056, -0.8005,  0.4891,  0.0269,  0.0519, -0.0313,  0.0292,  0.7314, -0.3619,
+             0.0032, -0.1011, -0.0813, -0.0382,  0.0320, -0.1481,  0.0837,  0.0223,  0.1587,
+						-0.0064,  2.3095, -1.5152,  0.0751, -0.0858, -0.5349,  0.1715, -2.4550,  1.1865,
+						-0.0171,  3.4017, -1.9862,  0.3242, -0.1450, -0.8043,  0.2952, -3.5284,  1.3575,
+						-0.0201,  7.1576, -6.3304,  0.5829,  0.1630, -0.3966,  0.5023, -7.1579,  5.2534,
+             0.0495,  1.5618, -6.0661,  0.8641,  1.1702,  1.7610,  0.9176, -2.1191,  5.4281,
+             0.0808, -5.3233, -1.1513,  0.9663,  1.6084,  2.7459,  0.8491,  4.7129,  1.1089;
+
+	//  vect = [geom.LWL./geom.DIVCAN^(1/3),
+	//   				geom.XFB./geom.LWL,
+	//   				geom.CPL,
+	//   				geom.DIVCAN.^(2/3)./geom.AW,
+	//   				geom.BWL./geom.LWL,
+	//   				geom.DIVCAN.^(2/3)./geom.SC,
+	//   				geom.XFB./geom.XFF,
+	//   				(geom.XFB./geom.LWL).^2,
+	//   				geom.CPL.^2]';
+	/// Vector with the physical quantities multiplying the
+	/// polynomial coefficients
+	Eigen::VectorXd vect(9);
+	vect << pParser_->get("LWL") / std::pow(pParser_->get("DIVCAN"),1./3),
+					pParser_->get("XBF") / pParser_->get("LWL") ,
+					pParser_->get("CPL"),
+					std::pow(pParser_->get("DIVCAN"),2./3) / pParser_->get("AW"),
+					pParser_->get("BWL") / pParser_->get("LWL"),
+					std::pow(pParser_->get("DIVCAN"),2./3) / pParser_->get("SC"),
+					pParser_->get("XFB") / pParser_->get("XFF"),
+					std::pow(pParser_->get("XFB") / pParser_->get("LWL"),2),
+					std::pow(pParser_->get("CPL"),2);
+
+	// Compute the the residuary resistance for all froude numbers
+	//  RrhD = (geom.DIVCAN.*phys.g.*phys.rho_w) .* ( coeff * vect' ) .* (geom.DIVCAN.^(1/3)./geom.LWL);
+	//  11x1        SCALAR                            11x9  *  9x1            SCALAR
+
+	// Residuary resistance values for each Froude number
+	Eigen::VectorXd RrhD = 	(pParser_->get("DIVCAN") * Physic::g * Physic::rho_w) *
+													( std::pow(pParser_->get("DIVCAN"),1./3) / pParser_->get("LWL") ) *
+													coeff * vect.transpose() ;
+
+	// Values of the froude number on which the coefficients of the
+	// residuary resistance are computed
+	Eigen::ArrayXd fnD(11);
+	fnD << 	.1, .15, .2, .25, .3, .35, .4, .45, .5, .55, .6;
+
+	// generate the cubic spline values for the coefficients
+	Eigen::ArrayXd RrhDArr=RrhD.array();
+	pInterpolator_.reset( new SplineInterpolator(fnD,RrhDArr) );
+
+}
+
+/// Destructor
+ResiduaryResistanceItem::~ResiduaryResistanceItem() {
+	// make nothing
+}
+
+/// Implement pure virtual method of the parent class
+void ResiduaryResistanceItem::update(int vTW, int aTW) {
+
+	// Call the parent class update to update the Froude number
+	ResistanceItem::update(vTW,aTW);
+
+	// Compute the residuary resistance for the current froude number
+	rrh_ = pInterpolator_->interpolate(fN_);
+
+}
 
--- main/VPPHydroItem.h
+++ main/VPPHydroItem.h
@@ -8,17 +8,19 @@ class ResistanceItem : public VPPItem {
 
 	public:
 
-		/// Constructor
-		ResistanceItem(VariableFileParser*, boost::shared_ptr<SailSet>);
-
 		/// Destructor
 		~ResistanceItem();
 
-	private:
+	protected:
+
+		/// Protected constructor (Avoid the instantiation of this class)
+		ResistanceItem(VariableFileParser*, boost::shared_ptr<SailSet>);
 
 		/// drag the pure virtual method of the parent class one step down
-		virtual void update(int vTW, int aTW) =0;
+		virtual void update(int vTW, int aTW);
 
+		/// Froude number
+		double fN_;
 
 };
 
@@ -49,6 +51,30 @@ class InducedResistanceItem : public ResistanceItem {
 		Eigen::ArrayXXd coeffA_, coeffB_;
 	  Eigen::ArrayXd phiD_, vectA_, Tegeo_;
 
+};
+
+//=================================================================
+
+class ResiduaryResistanceItem : public ResistanceItem {
+
+	public:
+
+		/// Constructor
+		ResiduaryResistanceItem(VariableFileParser*, boost::shared_ptr<SailSet>);
+
+		/// Destructor
+		~ResiduaryResistanceItem();
+
+	private:
+
+		/// Implement pure virtual method of the parent class
+		virtual void update(int vTW, int aTW);
+
+		/// Value of the induced resistance
+		double rrh_;
+
+		/// Interpolator that stores the residuary resistance curve for all froude numbers
+		boost::shared_ptr<SplineInterpolator> pInterpolator_;
 };
 
 #endif
