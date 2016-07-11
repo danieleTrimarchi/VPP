5510bfdf35d851da484a55f9dec14e2f6d80be76
dtrimarchi <devnull@localhost>
2016-07-11 08:54:02 +0200

    Regression is ready to accept point arrays

--- main/Regression.cpp
+++ main/Regression.cpp
@@ -1,47 +1,40 @@
 #include "Regression.h"
 #include <Eigen/Dense>
+#include "VPPException.h"
 
-// Constructor
+// Test Constructor
 Regression::Regression() {
 
-	// Set the test polynomial vector
-	Eigen::VectorXd polynomial(6);
-	// x^2 xy y^2 x y 1
-	polynomial << 0.01, .5, 0.03, .2, .3 , 1;
+	// Set the size of the coordinate matrices
+	x_.resize(3,3);
+	y_.resize(3,3);
+	z_.resize(3,3);
 
-	// Set the number of points m
-	size_t mx=3, my=3;
+	runTest();
 
-	// Set the coordinate arrays
-	Eigen::MatrixXd x(mx,my), y(mx,my), z(mx,my);
+}
 
-	Eigen::VectorXd factsx(mx);
-	factsx << 0.25, 0.5, 0.75;
-	Eigen::VectorXd factsy(my);
-	factsy << 0.25, 0.5, 0.75;
+// Constructor
+Regression::Regression(Eigen::MatrixXd& x, Eigen::MatrixXd& y, Eigen::MatrixXd& z ) :
+x_(x),
+y_(y),
+z_(z) {
 
-	Eigen::VectorXd coords(6);
+	if( (x_.rows() != y_.rows()) || (x_.rows() != z_.rows()) )
+		throw VPPException(HERE, "The coordinate arrays row size mismatch");
 
-	// Set the domain bounds
-	double xMin=0, xMax=1, yMin=0, yMax=1;
+	if( (x_.cols() != y_.cols()) || (x_.cols() != z_.cols()) )
+		throw VPPException(HERE, "The coordinate arrays col size mismatch");
 
-	// Loop on the mx*my points
-	for(size_t i=0; i<mx; i++){
-		for(size_t j=0; j<my; j++){
+}
 
-			// compute xi, yi, zi
-			x(i,j) = xMin + factsx(i) * ( xMax - xMin );
-			y(i,j) = yMin + factsy(j) * ( yMax - yMin );
 
-			// x^2 xy y^2 x y 1
-			coords << x(i,j)*x(i,j), x(i,j)*y(i,j), y(i,j)*y(i,j), x(i,j), y(i,j), 1;
+// Destructor
+Regression::~Regression() {
 
-			// compute z with the given polynomial. We now have the nx*ny points to
-			// be used to compute the regression and rebuild the initial polynomial
-			z(i,j) = coords.transpose() * polynomial;
+}
 
-		}
-	}
+Eigen::VectorXd Regression::compute() {
 
 	// Declare the coordinate matrix A to be inverted
 	Eigen::MatrixXd A(6,6);
@@ -50,34 +43,75 @@ Regression::Regression() {
 	Eigen::VectorXd b(6);
 
 	// Loop on the mx*my points
-	for(size_t i=0; i<mx; i++){
-		for(size_t j=0; j<my; j++){
+	for(size_t i=0; i<x_.rows(); i++){
+		for(size_t j=0; j<x_.cols(); j++){
 
 			// Define the coordinate vector for the current point
 			Eigen::VectorXd Q(6);
-			Q << x(i,j)*x(i,j),  x(i,j)*y(i,j), y(i,j)*y(i,j),x(i,j),y(i,j),1;
+			Q << x_(i,j)*x_(i,j), x_(i,j)*y_(i,j), y_(i,j)*y_(i,j), x_(i,j), y_(i,j), 1;
 
 			// Add the local A matrix to the global one
 			A += Q * Q.transpose();
 
 			// Add the local RHS to the global one
-			b += z(i,j) * Q;
+			b += z_(i,j) * Q;
 
 		}
 	}
 
 	// Compute the polynomial array p = inv(A) * b
-	Eigen::VectorXd p = A.colPivHouseholderQr().solve(b);
-
-	// Compare p with the original polynomial array
-	std::cout<< polynomial-p<<std::endl;
-
-
+	return A.colPivHouseholderQr().solve(b);
 
 }
 
-// Destructor
-Regression::~Regression() {
+void Regression::runTest() {
+
+
+	// Fill the test coordinate matrices :
+
+	// Set the test polynomial vector
+	Eigen::VectorXd polynomial(6);
+	// x^2 xy y^2 x y 1
+	polynomial << 0.01, .5, 0.03, .2, .3 , 1;
+
+	Eigen::VectorXd factsx(x_.rows());
+	factsx << 0.25, 0.5, 0.75;
+	Eigen::VectorXd factsy(y_.cols());
+	factsy << 0.25, 0.5, 0.75;
+
+	Eigen::VectorXd coords(6);
+
+	// Set the domain bounds
+	double xMin=0, xMax=1, yMin=0, yMax=1;
+
+	// Loop on the mx*my points
+	for(size_t i=0; i<x_.rows(); i++){
+		for(size_t j=0; j<x_.cols(); j++){
+
+			// compute xi, yi, zi
+			x_(i,j) = xMin + factsx(i) * ( xMax - xMin );
+			y_(i,j) = yMin + factsy(j) * ( yMax - yMin );
+
+			// x^2 xy y^2 x y 1
+			coords << x_(i,j)*x_(i,j), x_(i,j)*y_(i,j), y_(i,j)*y_(i,j), x_(i,j), y_(i,j), 1;
+
+			// compute z with the given polynomial. We now have the nx*ny points to
+			// be used to compute the regression and rebuild the initial polynomial
+			z_(i,j) = coords.transpose() * polynomial;
+
+		}
+	}
+
+	// Compute the polynomial array by polynomial regression on the given points
+	Eigen::VectorXd p = compute();
+
+	// Compare p with the original polynomial array
+	//std::cout<< polynomial-p<<std::endl;
+
+	if((polynomial-p).norm() > 1e-10)
+		throw VPPException(HERE,"\n\n==>> Regression test failed! <<==\n\n");
+	else
+		std::cout<<"\n\n=>> Regression test succeeded! <<==\n\n"<<std::endl;
 
 }
 
--- main/Regression.h
+++ main/Regression.h
@@ -5,19 +5,35 @@
 #include <stdio.h>
 #include <iostream>
 
+/// Aim of this class is to return a size-6 vector with the coefficients of a
+/// paraboloid that minimizes the distance from a given array of points.
+/// It is a regression in 3d.
+
 class Regression {
 
 	public:
 
-		/// Constructor
+		/// Test Constructor. Underneath calls compute, so it is standalone
 		Regression();
+
+		/// Constructor with point arrays. Requires calling compute()
+		Regression(Eigen::MatrixXd&, Eigen::MatrixXd&, Eigen::MatrixXd& );
 
 		/// Destructor
 		~Regression();
 
+		/// Compute the coefficient of the regression given the point array
+		Eigen::VectorXd compute();
+
 	private:
 
+		/// Run a test : define the points using a polynomial, then reconstruct
+		/// the regression and verify the coefficients are the initial ones. This
+		/// will be embedded into an autotest
+		void runTest();
 
+		/// Point array to be used to compute the regression
+		Eigen::MatrixXd x_, y_, z_;
 };
 
 #endif
