24c1ad74188db86d68202232563400deb3608d58
dtrimarchi <devnull@localhost>
2016-12-24 17:41:08 +0100

    Version.cpp must not be under version control, as it is automatically generated

--- main/versioning/Version.cpp
+++
@@ -1,7 +0,0 @@
-#include "Version.h"
-
-const char * currentBranch = "folderStructure";
-const char * currentRevNumber = "288+";
-const char * currentHash = "b09e46933f87+";
-const char * buildDate = "Sat Dec 24 15:27:31 2016";
-
