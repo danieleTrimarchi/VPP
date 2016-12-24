0b4a0c30c28a78ce6f117c0ceeac6db52c454952
dtrimarchi <devnull@localhost>
2016-12-24 15:31:27 +0100

    Estabilish the folder structure for the whole project

---
+++ main/versioning/Version.cpp
@@ -0,0 +1,7 @@
+#include "Version.h"
+
+const char * currentBranch = "folderStructure";
+const char * currentRevNumber = "288+";
+const char * currentHash = "b09e46933f87+";
+const char * buildDate = "Sat Dec 24 15:27:31 2016";
+
