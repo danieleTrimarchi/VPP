22717f17685f313ce1c1348004380798bb3d49cd
dtrimarchi <devnull@localhost>
2017-08-30 18:11:39 +0200

    Compiles (but does not run) with the std distribution of Qt

--- thirdParties.py
+++ thirdParties.py
@@ -9,7 +9,11 @@ class thirdParty(object) :
     def __init__(self):
         
         # Declare the root-folder for all third_parties
-        self.__rootDir__ = "/Users/dtrimarchi/third_party/"
+        self.__rootDir__ = "/Users/dtrimarchi/third_party"
+        
+        # Declare the src folder for the program to be compiled. Not ideal...
+        # This is done because the Qt .pc files currently live in the src tree... 
+        self.__VPPprogramSrcDir__ = "/Users/dtrimarchi/VPP"
 
         # Declare class members, to be filled by the children
         self.__includePath__= ""
@@ -18,6 +22,8 @@ class thirdParty(object) :
 
         self.__libs__= []
         self.__frameworks__= []
+        
+        self.__version__= ""
 
     # Public method, called by children once the 
     # members have been defined
@@ -44,8 +50,9 @@ class Eigen( thirdParty ) :
         # Call mother-class constructor
         super(Eigen,self).__init__()
         
+        self.__version__ = "3.2.7"
         # Declare class members, to be filled by the children
-        self.__includePath__= [self.__rootDir__ + 'eigen-3.2.7']
+        self.__includePath__= [self.__rootDir__ + 'eigen-' + self.__version__ ]
         self.__libpath__= []
         self.__libs__= []
     
@@ -60,9 +67,11 @@ class NLOpt( thirdParty ) :
         # Call mother-class constructor
         super(NLOpt,self).__init__()
         
+        self.__version__ = "2.4.2"
+
         # Declare class members, to be filled by the children
-        self.__includePath__= [self.__rootDir__ + 'nlopt-2.4.2/api']
-        self.__libpath__= [self.__rootDir__ + 'nlopt-2.4.2/libs']
+        self.__includePath__= [ os.path.join( self.__rootDir__,'nlopt-' + self.__version__,'api') ]
+        self.__libpath__= [ os.path.join( self.__rootDir__,'nlopt-' + self.__version__,'libs']
         self.__libs__= ['nlopt']
     
         self.__addTo__(env)
@@ -76,12 +85,14 @@ class IPOpt( thirdParty ) :
         # Call mother-class constructor
         super(IPOpt,self).__init__()
         
+        self.__version__ = "3.12.6"
+
         # Declare class members, to be filled by the children
-        self.__includePath__= [self.__rootDir__ + 'Ipopt-3.12.6/Ipopt/src/Interfaces',
-                               self.__rootDir__ +'Ipopt-3.12.6/include/coin']
-        self.__libpath__= [self.__rootDir__ + 'Ipopt-3.12.6/lib']
+        self.__includePath__= [os.path.join(self.__rootDir__,'Ipopt-'+self.__version__,'Ipopt/src/Interfaces'),
+                               os.path.join(self.__rootDir__,'Ipopt-'+self.__version__,'include/coin') ]
+        self.__libpath__= [ os.path.join(self.__rootDir__,'Ipopt-'+self.__version__,'lib') ]
         
-        self.__frameworksPath__= os.path.join(self.__rootDir__,"Ipopt-3.12.6/lib")
+        self.__frameworksPath__= os.path.join(self.__rootDir__,'Ipopt-'+self.__version__,'lib')
 
         # Define the list of libs
         self.__libs__= ['ipopt']
@@ -104,8 +115,10 @@ class Boost( thirdParty ) :
         # Call mother-class constructor
         super(Boost,self).__init__()
         
+        self.__version__ = "1_60_0"
+
         # Declare class members, to be filled by the children
-        self.__includePath__= [self.__rootDir__ + 'boost_1_60_0']
+        self.__includePath__= [ os.path.join( self.__rootDir__,'boost_'+self.__version__) ]
         self.__libpath__= []
         self.__libs__= []
     
@@ -119,10 +132,12 @@ class CppUnit( thirdParty ) :
         
         # Call mother-class constructor
         super(CppUnit,self).__init__()
-        
+
+        self.__version__ = "1.13.2"
+
         # Declare class members, to be filled by the children
-        self.__includePath__= [self.__rootDir__ + 'cppunit-1.13.2/build/include']
-        self.__libpath__= [self.__rootDir__ + 'cppunit-1.13.2/build/lib']
+        self.__includePath__= [ os.path.join(self.__rootDir__,'cppunit-'+self.__version__,'build/include') ]
+        self.__libpath__= [ os.path.join(self.__rootDir__,'cppunit-'+self.__version__,'build/lib') ]
         self.__libs__= ['cppunit']
     
         self.__addTo__(env)
@@ -136,8 +151,10 @@ class Qt( thirdParty ) :
         # Call mother-class constructor
         super(Qt,self).__init__()
         
+        self.__version__ = "5.9.1"
+
         # Overrride rootDir that is different for Qt as it is installed via homebrew
-        self.__rootDir__ = self.__rootDir__ + "Qt/5.9.1/clang64"
+        self.__rootDir__ = os.path.join(self.__rootDir__,'Qt',self.__version__,'clang_64')
 
         # Define the list of the libs (frameworks in this case)         
         self.__frameworks__= [
@@ -149,9 +166,10 @@ class Qt( thirdParty ) :
                               ]
 
         # Direcly customize the env for the package config
-        env['ENV']['PKG_CONFIG_PATH'] = [ os.path.join(self.__rootDir__,'Qt/5.9.1/clang64/lib/pkgconfig') ]    
+        env['ENV']['PKG_CONFIG_PATH'] = [ os.path.join(self.__VPPprogramSrcDir__,'site_scons','qtPkgConfig') ]    
         env['ENV']['PATH'] += ':/opt/local/bin:'+self.__rootDir__
 
+        # Remember rootDir is overwritten for Qt!
         env.Append( QT5DIR = self.__rootDir__ )
         
         # This is for http://stackoverflow.com/questions/37897209/qt-requires-c11-support-make-error
@@ -161,11 +179,13 @@ class Qt( thirdParty ) :
 
         env.EnableQt5Modules( self.__frameworks__ )
 
-        self.__includePath__= [ os.path.join(self.__rootDir__,'/include/QtWidgets') ]
+        self.__includePath__= [ os.path.join(self.__rootDir__,'lib/QtCore.framework/Versions/Current/Headers'),
+                                os.path.join(self.__rootDir__,'lib/QtWidgets.framework/Versions/Current/Headers'),
+                                os.path.join(self.__rootDir__,'lib/QtGui.framework/Versions/Current/Headers')
+                              ]
         
         # Returns the absolute path of the Qt Framework folder
-        self.__libpath__= [  os.path.join(self.__rootDir__, 'lib'), 
-                           "/usr/local/opt/qt5/lib"]
+        self.__libpath__= [ os.path.join(self.__rootDir__, 'lib') ]
 
         self.__addTo__(env)
         
