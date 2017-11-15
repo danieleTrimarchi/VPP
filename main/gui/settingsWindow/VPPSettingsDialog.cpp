90b1867579dfff560eb4bd9337bd8f786f8f1594
dtrimarchi <devnull@localhost>
2017-11-15 08:31:57 +0100

    Minor aesthetics

--- main/gui/settingsWindow/VPPSettingsDialog.cpp
+++ main/gui/settingsWindow/VPPSettingsDialog.cpp
@@ -3,25 +3,35 @@
 #include "TreeTab.h"
 #include <QtWidgets/QVBoxLayout>
 
+/// Ctor
 VPPSettingsDialog::VPPSettingsDialog(const QString &fileName, QWidget *parent)
 : QDialog(parent) {
 
+	// Instantiate a widget that will contain tabs
 	pTabWidget_ = new QTabWidget(this);
 
+	// Set the shape of the tab widget
 	pTabWidget_->setContentsMargins(0,0,0,0);
 	pTabWidget_->setTabShape(QTabWidget::TabShape::Triangular);
 
+	// Add a tree tab (a tab containing an item tree)
 	TreeTab* pTreeTab = new TreeTab(this);
 	pTabWidget_->addTab(pTreeTab, tr("VPP Settings"));
+
+	// Add a general tab (a tab containing genreral settings, not sure if we'll keep this)
 	pTabWidget_->addTab(new GeneralTab(this), tr("General"));
 
+	// Add the buttons 'OK' and "Cancel'
 	pButtonBox_ = new QDialogButtonBox(	QDialogButtonBox::Ok |
 			QDialogButtonBox::Cancel);
 
+	// Connect signals to the buttons
 	connect(pButtonBox_, &QDialogButtonBox::accepted, this, &QDialog::accept);
 	connect(pButtonBox_, &QDialogButtonBox::rejected, this, &QDialog::reject);
 
-	QVBoxLayout *mainLayout = new QVBoxLayout;
+	// Instantiate a VBoxLayout, the tab are on top and the buttons on the bottom
+	// Set this layout for the Dialog
+	QVBoxLayout* mainLayout = new QVBoxLayout;
 	mainLayout->addWidget(pTabWidget_);
 	mainLayout->addWidget(pButtonBox_);
 	setLayout(mainLayout);
@@ -29,5 +39,6 @@ VPPSettingsDialog::VPPSettingsDialog(const QString &fileName, QWidget *parent)
 	// Resize the window in order to show as much as possible
 	resize(pTreeTab->width(),pTreeTab->height());
 
+	// Set the title of this widget
 	setWindowTitle(tr("Tab Dialog"));
 }
--- main/gui/settingsWindow/VPPSettingsDialog.h
+++ main/gui/settingsWindow/VPPSettingsDialog.h
@@ -1,23 +1,29 @@
 #ifndef VPP_SETTINGS_DIALOG_H
 #define VPP_SETTINGS_DIALOG_H
 
-#include <QDialog>
+#include <QtWidgets/QDialog>
 #include <QtWidgets/QDialogButtonBox>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QTabWidget>
 
+/// Implements the VPP Settings Dialog, or the settings window where it is
+/// possible to edit all properties requested to run the VPP
 class VPPSettingsDialog : public QDialog {
 
 		Q_OBJECT
 
 	public:
 
+		/// Ctor
 		explicit VPPSettingsDialog(const QString &fileName, QWidget *parent = 0);
 
 	private:
 
+		/// The Dialog has a tab structure. Tabs are added to the tabWidget
 		QTabWidget* pTabWidget_;
+
+		/// The Dialog also has two buttons to save or cancel the editing
 		QDialogButtonBox* pButtonBox_;
 };
 
